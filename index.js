07a3f9b27ba83f84134ebe612ad8da0c7eae4277
Robby <doubtfullink53@gmail.com>
2023-01-14 12:38:52 -0500

    update javascript to work with touchscreens and mouse

--- index.js
+++ index.js
@@ -1,82 +1,64 @@
-document.querySelector('input').addEventListener('input', e => document.getElementById('size1').innerText = e.target.value );
-
-document.querySelector('input').addEventListener('input', e => document.getElementById('size2').innerText = e.target.value );
-
-
-
-
 const container = document.getElementById("container");
+const colorPicker = document.querySelector('#input-color');
+let color = colorPicker.value;
+let isMouseDown = false;
 
 // make div grids 
 function makeRows(row, col) {
-  
   container.style.setProperty('--grid-rows', row);
   container.style.setProperty('--grid-cols', col);
   
   for (i = 0; i < (row * col ); i++) {
     let cell = document.createElement("div");
-    // cell.innerText = (i + 1);
     container.appendChild(cell).className = "grid-item";
-  };
-};
-
-
-function colorPicker(){
-  
-}
-
-
-// click and move mouse on grid to color each grid tile
-function mouseEvent(){
-
-  const colorPicker = document.querySelector('#input-color');
-
-  let color = colorPicker.value;
-
-  colorPicker.addEventListener('change', function() {
-    color = colorPicker.value
-  });
-   let mousePressedDown;
- 
-$(document).mousedown(function() {
-  mousePressedDown = true;      
-})
-.mouseup(function() {
-  mousePressedDown = false;    
-});
-
-$(".grid-item").mouseover(function(){
-  if(mousePressedDown) {       
-     $(this).css({background: color});
   }
-});
-
 }
-
-
-
-
-let row = 12
-let col = 12
-
-makeRows(row, col);
-
 
 // change pixel size , remake grid 
 function getSize(){
-  
-  let row =   document.getElementById('size1').innerText
-  let col =   document.getElementById('size2').innerText
- 
-  $("div").remove(".grid-item");
-  makeRows(parseInt(row), parseInt(col) );
+  let row =   document.getElementById('size1').innerText;
+  let col =   document.getElementById('size2').innerText;
 
-   mouseEvent()
-   
+  $(".grid-item").remove();
+  makeRows(parseInt(row), parseInt(col));
 }
 
-mouseEvent();
+colorPicker.addEventListener('change', function() {
+  color = colorPicker.value
+});
 
+container.addEventListener('mousedown', e => {
+  isMouseDown = true;
+});
 
+container.addEventListener('mousemove', e => {
+  if (isMouseDown && e.target.classList.contains("grid-item")) {
+    e.target.style.background = color;
+  }
+});
 
+container.addEventListener('mouseup', e => {
+  isMouseDown = false;
+});
 
+container.addEventListener('touchstart', e => {
+  e.preventDefault();
+  if (e.target.classList.contains("grid-item")) {
+    e.target.style.background = color;
+  }
+});
+
+container.addEventListener('touchmove', e => {
+  e.preventDefault();
+  if (e.target.classList.contains("grid-item")) {
+    e.target.style.background = color;
+  }
+});
+
+container.addEventListener('touchend', e => {
+  e.preventDefault();
+});
+
+let row = 12;
+let col = 12;
+makeRows(row, col);
